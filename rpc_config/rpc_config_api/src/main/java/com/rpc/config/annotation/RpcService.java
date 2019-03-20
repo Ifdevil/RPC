7bd8a0d1284847b625b4c925f1cf8a6a0c2bac1f
ifdevil <384867771@qq.com>
2019-03-20 17:45:18 +0800

    service annotation

---
+++ rpc_config/rpc_config_api/src/main/java/com/rpc/config/annotation/RpcService.java
@@ -0,0 +1,16 @@
+package com.rpc.config.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 调用服务注解
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface RpcService {
+
+    Class<?> interfaceClass() default void.class;
+}
