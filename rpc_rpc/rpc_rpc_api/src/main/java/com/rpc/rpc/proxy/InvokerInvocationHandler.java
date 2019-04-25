8b31c9a4c296b68ecc12f29d75e8773dd00fe01c
ifdevil <384867771@qq.com>
2019-04-25 18:01:54 +0800

    invocationHandler

---
+++ rpc_rpc/rpc_rpc_api/src/main/java/com/rpc/rpc/proxy/InvokerInvocationHandler.java
@@ -0,0 +1,12 @@
+package com.rpc.rpc.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class InvokerInvocationHandler implements InvocationHandler {
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
